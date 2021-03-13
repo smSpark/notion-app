c52f97d1217f511a47bd011a6db825272ed522fd
smSpark <you@gmail.com>
2021-03-13 23:55:51 +0300

    loader+

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Contacts } from "./pages/Contacts/Contacts";
+import { SideWindows } from "./pages/SideWindows/SideWindows";
+
 
 const App: React.FC = () => (
   <>
     <Contacts />
+    <SideWindows />
   </>
 );
 
